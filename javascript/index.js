df901654ae3dc29739295c27a3d764e847fa7c53
avilesha <avileshagovender95@gmail.com>
2025-04-22 11:47:33 +0200

    corrected html and javascript

--- javascript/index.js
+++ javascript/index.js
@@ -18,21 +18,27 @@ parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
 parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");}
 }
 
-function updateCity(event){
- let cityTimeZone = event.target.value;
-if (cityTimeZone = "current"){
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  console.log(cityTimeZone);
+  if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
-}
- let cityTime = moment().tz(cityTimeZone);
- let citiesElement = document.querySelector("#cities");
- citiesElement.innerHTML = `
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
  <div class="city">
     <div>
-<h2>${cityTimeZone}</h2>
+<h2>${cityName}</h2>
 <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-<div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
- </div>`;
+<div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+ </div>
+
+ `;
 }
  
  updateTime()
